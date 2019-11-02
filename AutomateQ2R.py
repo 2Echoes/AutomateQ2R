9d319a2fd98cec505190deac3425c2c9b555220a
2Echoes <floric.slimani@live.fr>
2019-11-02 09:43:24 +0100

    Work in progress

--- AutomateQ2R.py
+++ AutomateQ2R.py
@@ -2,46 +2,83 @@ import numpy as np
 
 class SpinGrid():
 
-    def __init__(self,hauteur,largeur,temp_ini):
-        self.__hauteur = hauteur
-        self.__largeur = largeur
-        self.__temperature = temp_ini
-        # self.__energie
-        # self.__probabilite = 
-        self.__grid = self.__grid = np.random.choice((-1,1),(hauteur,largeur))
+    J = 1 # Normalisée en l'absence de champ magnétique extérieur pour tout i,j J = J(ij)
+    kb = 1 #Constante de Boltzmann normalisée
+
+    def __init__(self,height,width,ini_temp):
+        self.__height = height
+        self.__width = width
+        self.__temperature = ini_temp
+        self.__spinNumber = height*width
+        self.__spingen = self.spin_generator()
 
 
     @property
-    def hauteur(self):
-        return self.__hauteur
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self,hgt):
+        self.__height = hgt
+
     @property
-    def largeur(self):
-        return self.__largeur
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self,wdth):
+        self.__width = wdth
+
     @property
     def temperature(self):
         return self.__temperature
+    @temperature.setter
+    def temperature(self,temp):
+        self.__temperature = temp
+
     @property
-    def grid(self):
-        return self.__grid
+    def spinNumber(self):
+        return self.__spinNumber
+    @spinNumber.setter
+    def spinNumber(self,number):
+        if number == self.height * self.width:
+            self.__spinNumber = number
 
-    @grid.setter
-    def grid(self,hauteur,largeur):
-        self.__grid = np.random.choice((-1,1),(hauteur,largeur))
+    def print(self):
+        print("height = ",self.height," width = ",self.width,"\nSpin number = ",self.spinNumber,"\ntemperature = ",self.temperature)
 
-        
-
-        
-"""     @property
-    def energie(self):
-        return self.__energie
-    @property
-    def probabilite(self):
-        return self.__probabilite """
+    def spin_generator(self):
+        Spingrid = np.zeros(self.height,self.width)
+        for line in range(1,self.height+1):
+            for col in range(1,self.width+1):
+                Spingrid[line,col] = Spin(line,col,grid=self)
     
+    
+class Spin():
+    
+    def __init__(self,coord,grid):
+        self.__value = np.random.choice([-1,1])
+        self.__coordinates = coord
+        self.__grid = grid
+
+    @property
+    def value(self):
+        return self.__value
+    @value.setter
+    def value(self,SpinV):
+        self.__value = SpinV
+    
+    @property
+    def coordinates(self):
+        return self.__coordinates
+    @coordinates.setter
+    def coordinates(self,coord):
+        if (type(coord) == tuple or type(coord) == list or type(coord) == np.ndarray) and len(coord) == 2:
+            self.__coordinates = coord
+
+    def print(self):
+        print('coordinates = ',self.coordinates,'   Spin = ',self.__value)
+    
+    def neighboorsum(self):
+        sum = 
 
 
-
-
-if __name__=='__main__':
-    A = SpinGrid(10,10,100)
-    print(A.grid)
+if __name__ == '__main__':    
